c2827fd875e74e28014725ef7d8ceaa7dd09039b
DESKTOP-U2BJ55P\Sander <sanderxavalon@gmail.com>
2022-04-25 12:16:03 +0800

    新增springmvc restful釋例

---
+++ springboot-springmvc-restful/src/main/java/org/iii/contoller/PostParametersController.java
@@ -0,0 +1,66 @@
+package com.torch.springbootspringmvcrestful.contoller;
+
+import com.torch.springbootspringmvcrestful.valueobject.User;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+@RestController
+@RequestMapping("/prefix")
+public class PostParametersController {
+
+    // 1. HttpServletRequest直接拿參數
+    @RequestMapping(path = "/OldFashionWay", method = RequestMethod.POST)
+    public void getMethodHttpServletRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println(request.getParameter("id"));
+        Writer writer = response.getWriter();
+        writer.write("success");
+        writer.flush();
+        writer.close();
+    }
+
+    // 2. Method Parameter 對應 Request Parameter
+    // 順序無關，如果無參嘖null
+    @RequestMapping(path = "/getParamsWithoutAnnotation", method = RequestMethod.POST)
+    public String getParamsWithoutAnnotation(String id, String user){
+        return id + user;
+    }
+    // 3. RequestParam
+    @RequestMapping(path = "/getParams", method = RequestMethod.POST)
+    public String getMethodWithRequestParams(@RequestParam("id") String id){
+        return id;
+    }
+
+    // RequestParam, 可以設定為參數非必填
+    @RequestMapping(path = "/requiredParams", method = RequestMethod.POST)
+    public String getMethodWithNotRequiredParams(@RequestParam(name = "id", required = false) String id){
+        return id;
+    }
+
+    // RequestParam, 可以設定為參數非必填
+    @RequestMapping(path = "/defaultValue", method = RequestMethod.POST)
+    public String getMethodWithDefaultValue(@RequestParam(name = "id", defaultValue = "123") String id){
+        return id;
+    }
+
+    // 4. Value Object直接映射
+    @RequestMapping(path = "/getParamsReflectOnObject", method = RequestMethod.POST)
+    public String getParamsReflectOnObject(User user){
+        return user.getId()+ user.getName();
+    }
+
+    // 5. PathVariable, 試試看如果與上面"/get/*"路徑重疊會如何
+    @RequestMapping(path = "/get/{id}/{number}", method = RequestMethod.POST)
+    @ResponseBody
+    public String getMethodDynamicURI(@PathVariable("id") String id, @PathVariable("number") int number){
+        return id + number;
+    }
+
+    @RequestMapping(path = "/{action:test1|test2}", method = RequestMethod.POST)
+    public String getMethodAntPathMatcher(@PathVariable("action") String action){
+        return action;
+    }
+}
---
+++ springboot-springmvc-restful/src/main/java/org/iii/contoller/RestfulController.java
@@ -0,0 +1,53 @@
+package com.torch.springbootspringmvcrestful.contoller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.standard.Media;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+// @RestController is a stereotype annotation that combines @ResponseBody and @Controller.
+
+@RestController
+@RequestMapping("/prefix")
+public class RestfulController {
+
+    @RequestMapping(path = "/get", method = RequestMethod.GET)
+    public String getMethod(){
+        return "success";
+    }
+
+    @RequestMapping(path = "/get/*", method = RequestMethod.GET)
+    public String getMethodWithMultiPath(){
+        return "success";
+    }
+
+    // 用瀏覽器打開看是否會以PDF形式開啟
+    @RequestMapping(path = "/getPDF", method = RequestMethod.GET, produces = MediaType.APPLICATION_PDF_VALUE)
+    public String getMethodProduceJSON(){
+        return "success";
+    }
+
+    // 刪除consumes後觀察Response Header會有什麼改變
+    @RequestMapping(path = "/sendJSON", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public String getMethodConsumeJSON(){
+        return "success";
+    }
+
+    // 用postman的Body中改變content-type會有甚麼改變
+    @RequestMapping(path = "/sendPlainText", method = RequestMethod.GET, headers = "content-type=text/plain")
+    public String getMethodContentType(){
+        return "success";
+    }
+
+    // 限定parameters在某種特定情況下才接收
+    // 注意params內的內容不可空白分隔，如同URL傳過來的一樣
+    @RequestMapping(path = "/specifyParameters", method = RequestMethod.GET, params = "id=100")
+    public String getMethodSpecifyParameters(@RequestParam("id") String id){
+        return "success";
+    }
+
+}
